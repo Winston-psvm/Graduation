ee3f2dc313313eebfcd32415ac840cae4113a0a5
Alex <clearwinstonsigi@gmail.com>
2021-10-27 14:00:28 +0300

    add and implementation DB

--- src/main/java/myproject/graduation/GraduationApplication.java
+++ src/main/java/myproject/graduation/GraduationApplication.java
@@ -1,13 +1,18 @@
 package myproject.graduation;
 
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GraduationApplication {
+public class GraduationApplication implements ApplicationRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(GraduationApplication.class, args);
     }
 
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+    }
 }
