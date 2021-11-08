fceb856505b224795df574d0cbd8a71797b6707f
Alex <clearwinstonsigi@gmail.com>
2021-11-08 15:29:35 +0300

    add DTo

---
+++ src/main/java/myproject/graduation/to/BaseTo.java
@@ -0,0 +1,20 @@
+package myproject.graduation.to;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import myproject.graduation.util.HasId;
+
+@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Data
+public abstract class BaseTo implements HasId {
+    @Schema(accessMode = Schema.AccessMode.READ_ONLY)
+    protected Integer id;
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + ":" + id;
+    }
+}
