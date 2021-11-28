051a84d1d5f61c711ba6431bf401c5483a1b3ea7
Alex <clearwinstonsigi@gmail.com>
2021-11-28 12:58:57 +0300

    fix

--- src/test/java/myproject/graduation/web/AbstractControllerTest.java
+++ src/test/java/myproject/graduation/web/AbstractControllerTest.java
@@ -1,4 +1,5 @@
 package myproject.graduation.web;
+
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
--- src/test/java/myproject/graduation/web/TestData.java
+++ src/test/java/myproject/graduation/web/TestData.java
@@ -13,12 +13,10 @@ public class TestData {
     public static final int USER_ID = 2;
     public static final int ADMIN_ID = 1;
     public static final int RESTAURANT_ID = 1;
-    public static final int MENU_ID = 1;
     public static final String USER_MAIL = "user@yandex.ru";
     public static final String ADMIN_MAIL = "admin@gmail.com";
 
     public static final User user = new User(USER_ID, "User", USER_MAIL, "password", Role.USER);
-    public static final User admin = new User(ADMIN_ID, "Admin", ADMIN_MAIL, "admin", Role.ADMIN, Role.USER);
     public static final Restaurant rest = new Restaurant(1,"Kebab", "Lithuania", "+375882558899");
 
 
